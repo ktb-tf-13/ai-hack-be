d273be775fffc3ffe6baf158b4d034db1e7b435b
Hyeseong-Myeong <a01025819869@gmail.com>
2025-12-17 17:38:22 +0900

    setting: project init

---
+++ src/main/java/com/ktb/ktb_tf_13_hack_be/KtbTf13HackBeApplication.java
@@ -0,0 +1,13 @@
+package com.ktb.ktb_tf_13_hack_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KtbTf13HackBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KtbTf13HackBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ktb/ktb_tf_13_hack_be/KtbTf13HackBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktb.ktb_tf_13_hack_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KtbTf13HackBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
