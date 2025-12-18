f40e2a2f5dc234728c2c6eecba102beac6808a69
Hyeseong-Myeong <a01025819869@gmail.com>
2025-12-18 20:29:56 +0900

    fix: router path

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from openai import AsyncOpenAI
-from app.routes import challenge_router  # [추가] 위에서 만든 라우터 import
+from app.routes import challenge_router
 
 
 app = FastAPI(title="Backend + AI Server",
@@ -88,7 +88,7 @@ app.add_middleware(
 
 api_router = APIRouter()
 api_router.include_router(router=user_router.router)
-app.include_router(challenge_router.router)
+api_router.include_router(router=challenge_router.router)
 
 
 app.include_router(api_router)
